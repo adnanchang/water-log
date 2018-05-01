f995be52edf8448379e44b55301579818aa82e60
Muhammad Adnan <m.adnanchangezi@gmail.com>
2018-05-01 19:12:33 +0200

    first commit

---
+++ server-sails/api/controllers/BoatController.js
@@ -0,0 +1,11 @@
+/**
+ * BoatController
+ *
+ * @description :: Server-side logic for managing boats
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
